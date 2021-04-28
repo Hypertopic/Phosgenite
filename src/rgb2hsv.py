133d107077f9ae5a574a40db2ed082f88458e099
lKetchup <antoinepradel@neuf.fr>
2021-04-28 17:02:14 +0200

    add case if path is not absolute

--- src/rgb2hsv.py
+++ src/rgb2hsv.py
@@ -10,16 +10,24 @@ class Rgb2hsv:
     def transform_folder(self, folder_input, folder_output):
         _, _, filenames = next(os.walk(folder_input))
 
-        os.chdir(folder_input)
-
         for file in filenames:
-            img = cv2.imread(file)
+            
+            #if path is not absolute
+            '''
+            abs_path = folder_input + file
+            f = os.path.abspath(f)
+            f.replace('/', '\\')
+            '''
+            
+            img = cv2.imread(folder_input + file)
             hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
             cv2.imwrite(folder_output + file, hsv[:, :, 2])
+            
+        #os.chdir('../src')
 
     def transform_img(self, file):
         img = cv2.imread(file)
+        print(img)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
         return hsv[:, :, 2]
