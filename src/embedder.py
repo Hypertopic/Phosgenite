8803c498570034e9c64784765eb70b7d2e1c4b61
lKetchup <antoinepradel@neuf.fr>
2021-04-28 14:51:47 +0200

    Separation bettwen embedding train images with labels and test images without label

--- src/embedder.py
+++ src/embedder.py
@@ -1,19 +1,41 @@
 from orangecontrib.imageanalytics.image_embedder import ImageEmbedder
 import os
-
+import pandas as pd
 
 class Embedder:
     def __init__(self, path):
         self.path = path
         self.path_array = []
+        self.name = []
 
     def find_path(self):
         with os.scandir(self.path) as dirs:
             for entry in dirs:
                 self.path_array.append(self.path + entry.name)
 
+    def train_find_path(self):
+        with os.scandir(self.path) as dirs:
+            for entry in dirs:
+                self.path_array.append(self.path + entry.name)
+                name = entry.name.split('.')
+                self.name.append(name[0])
+
     def embedding(self):
         self.find_path()
         with ImageEmbedder(model='painters') as emb:
             embeddings = emb(self.path_array)
         return embeddings
+
+
+    def train_embedding(self):
+        self.train_find_path()
+        with ImageEmbedder(model='painters') as emb:
+            embeddings = emb(self.path_array)
+            e = pd.DataFrame(embeddings)
+            e["name"] = self.name
+        return e
+
+emb = Embedder("../data/SJ/Vitraux baies/").train_embedding()
+
+
+print(emb)
