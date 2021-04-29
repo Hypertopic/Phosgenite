98b05d28ada949c0a27f975ba580d821d4ddbd8d
lKetchup <antoinepradel@neuf.fr>
2021-04-29 14:31:25 +0200

    write transformed image and clean classe

--- src/rgb2hsv.py
+++ src/rgb2hsv.py
@@ -1,7 +1,6 @@
 import cv2
 import os
 
-
 class Rgb2hsv:
 
     def __init__(self):
@@ -9,24 +8,18 @@ class Rgb2hsv:
 
     def transform_folder(self, folder_input, folder_output):
         _, _, filenames = next(os.walk(folder_input))
-
         for file in filenames:
-            
-            #if path is not absolute
+            # if path is not absolute
             '''
             abs_path = folder_input + file
             f = os.path.abspath(f)
             f.replace('/', '\\')
             '''
-            
             img = cv2.imread(folder_input + file)
             hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
             cv2.imwrite(folder_output + file, hsv[:, :, 2])
-            
-        #os.chdir('../src')
 
-    def transform_img(self, file):
+    def transform_img(self, file, output):
         img = cv2.imread(file)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-        return hsv[:, :, 2]
+        cv2.imwrite(output, hsv[:, :, 2])
