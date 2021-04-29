80ea7d02e28cc9d008618c8e60ed977eb4435a88
lKetchup <antoinepradel@neuf.fr>
2021-04-29 15:07:33 +0200

    Create main file to call the model

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from TSNE_KNN_model import TSNE_KNN_model
+
+import time
+
+start = time.time()
+TSNE_KNN_model("C:/Users/antoi/Documents/UTT/ISI 4/PE/Phosgenite/src/data/SJ/test images/SJ 019 3.png")
+end = time.time()
+print("Exdecution time: ", end - start)
