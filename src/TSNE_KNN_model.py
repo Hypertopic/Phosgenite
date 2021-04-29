ad52590d1e931c5bca35fe083517523e333465a2
lKetchup <antoinepradel@neuf.fr>
2021-04-29 14:56:09 +0200

    simplify transform train and transform test by adding simple parameters

--- src/TSNE_KNN_model.py
+++ src/TSNE_KNN_model.py
@@ -17,34 +17,36 @@ class TSNE_KNN_model:
         self.test = img
 
     def transform_train(self):
-
-        rgb2hsv = Rgb2hsv().transform_folder(self.cwd + "/data/SJ/Vitraux baies/", self.cwd + "/data/SJ/Vitraux baies hsv/")
-        self.train = Embedder(self.cwd + "/data/SJ/Vitraux baies hsv/").train_embedding()
+        Rgb2hsv().transform_folder(self.cwd + "/data/SJ/Vitraux baies/", self.cwd + "/data/SJ/Vitraux baies hsv/")
+        self.train = Embedder().train_embedding(self.cwd + "/data/SJ/Vitraux baies hsv/")
+        print(self.train)
 
     def transform_test(self):
-        self.test = Rgb2hsv().transform_img(self.test)
-        self.test = Embedder("C:/Users/antoi/Documents/UTT/ISI 4/PE/Phosgenite/src/data/SJ/test images/").embedding()
+        self.test = Rgb2hsv().transform_img(self.test, 'test.png')
+        self.test = Embedder().embedding('test.png')
+        print(self.test)
 
     def tsne(self):
         y_train = self.train["name"].values
         x_train = self.train.drop("name", axis=1).values
 
         X = np.concatenate([x_train, self.test])
-
+        print(pd.DataFrame(X))
         tsne = TSNE(n_components=2, perplexity=10, early_exaggeration=1)
         x = tsne.fit_transform(X)
         df = pd.DataFrame(x)
 
         self.train = df[:(len(y_train))]
+        self.test = df[(len(y_train)):]
         self.train["category"] = y_train
-
-        test = df[(len(y_train)) + 1:]
 
         print(self.train)
 
-model = TSNE_KNN_model("C:/Users/antoi/Documents/UTT/ISI 4/PE/Phosgenite/src/data/SJ/test images/SJ 000 1.png")
+
+model = TSNE_KNN_model("C:/Users/antoi/Documents/UTT/ISI 4/PE/Phosgenite/src/data/SJ/test images/SJ 000 5.png")
 model.transform_train()
 model.transform_test()
 model.tsne()
+
 
 
