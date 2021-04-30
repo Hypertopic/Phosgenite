6756b891619fcc126748d5b87b30c9355688f9c6
lKetchup <antoinepradel@neuf.fr>
2021-04-30 17:42:21 +0200

    change self.name to self.category to match train dataset

--- src/embedder.py
+++ src/embedder.py
@@ -5,14 +5,14 @@ import pandas as pd
 class Embedder:
     def __init__(self):
         self.path_array = []
-        self.name = []
+        self.category = []
 
     def find_path(self, folder_path):
         with os.scandir(folder_path) as dirs:
             for entry in dirs:
                 self.path_array.append(folder_path + entry.name)
                 name = entry.name.split('.')
-                self.name.append(name[0])
+                self.category.append(name[0])
 
     def embedding(self, path):
         with ImageEmbedder(model='painters') as emb:
@@ -24,5 +24,5 @@ class Embedder:
         with ImageEmbedder(model='painters') as emb:
             embeddings = emb(self.path_array)
             e = pd.DataFrame(embeddings)
-            e["name"] = self.name
+            e["category"] = self.category
         return e
