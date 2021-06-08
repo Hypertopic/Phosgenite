f9afb1d91d059dddb41e70a09808576f175b623c
lKetchup <antoinepradel@neuf.fr>
2021-06-08 10:32:51 +0200

    ajout doc

--- src/main.py
+++ src/main.py
@@ -3,6 +3,11 @@ from TSNE_KNN_model import TSNE_KNN_model
 import time
 import os
 
+'''
+Main function
+call the function TSNE_KNN_model and pass the image to recognize
+return the label of the image
+'''
 def find_label(img):
     start = time.time()
     lab = TSNE_KNN_model(os.getcwd() + img)
@@ -11,5 +16,5 @@ def find_label(img):
     return lab
 
 
-#exemple
+#exemple of use
 find_label("/data/SJ/test images/SJ 000 1.jpg")
